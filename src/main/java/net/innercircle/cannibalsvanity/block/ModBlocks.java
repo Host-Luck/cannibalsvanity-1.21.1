ed9a9cee85586df7d7bf75fbac9a4c19028484b6
Host-Luck <vanderford.trey@gmail.com>
2025-11-29 15:24:56 -0500

    Guestt

--- src/main/java/net/innercircle/cannibalsvanity/block/ModBlocks.java
+++ src/main/java/net/innercircle/cannibalsvanity/block/ModBlocks.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -19,7 +20,15 @@ public class ModBlocks {
 
     public static final DeferredBlock<Block> TOUGHFLESH_BLOCK = registerBlock("toughflesh_block",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .sound(SoundType.WET_SPONGE).strength(2f)));
+                    .sound(SoundType.WET_SPONGE)
+                    .strength(1.5f)));
+    public static final DeferredBlock<Block> CORPSE_BLOCK = registerBlock("corpse_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .sound(SoundType.FUNGUS)
+                    .strength(2f)));
+    public static final DeferredBlock<StairBlock> CORPSE_STAIRS = registerBlock("corpse_stairs",
+            () -> new StairBlock(ModBlocks.CORPSE_BLOCK.get().defaultBlockState(),
+                    BlockBehaviour.Properties.of().strength(2f)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
